a65cb103aa24cd9baa3e84ffd658d08bee0c7350
Xalalau Xubilozo <xalalau@outlook.com>
2020-06-11 18:38:16 -0300

    Consertei os halos na entidade teste

--- lua/entities/d_box.lua
+++ lua/entities/d_box.lua
@@ -9,21 +9,27 @@ ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 ENT.Category = "Dom's Advanced Delivery system"
 AddCSLuaFile()
 
+function ENT:Initialize()
+    self:SetModel("models/Combine_Helicopter/helicopter_bomb01.mdl")
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    local phys = self:GetPhysicsObject()
+    if phys:IsValid() then phys:Wake() end
+    phys:EnableMotion( false )
+
+    if SERVER then
+        self:SetUseType(SIMPLE_USE)
+    else
+        hook.Add( "PreDrawHalos", "AddPropHalos" .. tostring(self), function()
+            halo.Add( { self } , Color( 255, 0, 0, 255 ), 5, 5, 5,true,true )
+        end )
+    end
+end
+
 if SERVER then
 
-	function ENT:Initialize()
-		self:SetModel("models/Combine_Helicopter/helicopter_bomb01.mdl")
-		self:PhysicsInit(SOLID_VPHYSICS)
-		self:SetMoveType(MOVETYPE_VPHYSICS)
-		self:SetSolid(SOLID_VPHYSICS)
-		local phys = self:GetPhysicsObject()
-		if phys:IsValid() then phys:Wake() end
-		phys:EnableMotion( false )
-		self:SetUseType(SIMPLE_USE)
-	end
-
-
-	function ENT:Use( act, ply )
+    function ENT:Use( act, ply )
 		print(s)
 	end
 
@@ -37,27 +43,22 @@ end
 
 if CLIENT then
 
-local matBall = Material( "sprites/sent_ball" )
+    local matBall = Material( "sprites/sent_ball" )
 
-function ENT:Draw()
+    function ENT:Draw()
 
-	render.SetMaterial( matBall )
+        render.SetMaterial( matBall )
 
-	local pos = self:GetPos()
-	local lcolor = render.ComputeLighting( pos, Vector( 0, 1, 1 ) )
-	local c = (Color(255,0,0))
+        local pos = self:GetPos()
+        local lcolor = render.ComputeLighting( pos, Vector( 0, 1, 1 ) )
+        local c = (Color(255,0,0))
 
-	lcolor.x = c.r * ( math.Clamp( lcolor.x, 0, 1 ) + 0.5 ) * 255
-	lcolor.y = c.g * ( math.Clamp( lcolor.y, 0, 1 ) + 0.5 ) * 255
-	lcolor.z = c.b * ( math.Clamp( lcolor.z, 0, 1 ) + 0.5 ) * 255
+        lcolor.x = c.r * ( math.Clamp( lcolor.x, 0, 1 ) + 0.5 ) * 255
+        lcolor.y = c.g * ( math.Clamp( lcolor.y, 0, 1 ) + 0.5 ) * 255
+        lcolor.z = c.b * ( math.Clamp( lcolor.z, 0, 1 ) + 0.5 ) * 255
 
-	local size = math.Clamp( 10, 4, 20 )
-	render.DrawSprite( pos, size, size, Color( lcolor.x, lcolor.y, lcolor.z, 255 ) )
-	
-	hook.Add( "PreDrawHalos", "AddPropHalos", function()
-   		 halo.Add( self, Color( 255, 0, 0 ), 5, 5, 5,true,true )
-	end )
-end
+        local size = math.Clamp( 10, 4, 20 )
+        render.DrawSprite( pos, size, size, Color( lcolor.x, lcolor.y, lcolor.z, 255 ) )
+    end
 
-	
 end
